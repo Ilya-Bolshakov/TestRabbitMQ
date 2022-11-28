094804bfa3a0d2e68046e489362f643738115573
Илья Большаков <62346242+Ilya-Bolshakov@users.noreply.github.com>
2022-11-28 09:15:59 +0300

    Добавьте файлы проекта.

---
+++ Receive/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.50.10",
+                Port = 5672,
+                AutomaticRecoveryEnabled = true,
+                NetworkRecoveryInterval = new TimeSpan(0, 1, 0),
+                RequestedHeartbeat = new TimeSpan(0, 10, 0),
+                UserName = "guest",
+                Password = "guest"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("PSClientPays", true ? ExchangeType.Fanout : ExchangeType.Direct, true, false, null);
+
+                channel.QueueDeclare(queue: "PSClientPays",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+                
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "PSClientPays",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+
+
+namespace Send
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var factory = new ConnectionFactory() 
+            {
+                HostName = "192.168.50.10",
+                Port = 5672,
+                AutomaticRecoveryEnabled = true,
+                NetworkRecoveryInterval = new TimeSpan(0, 1, 0),
+                RequestedHeartbeat = new TimeSpan(0, 10, 0),
+                UserName = "guest",
+                Password = "guest"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("PSClientPays", true ? ExchangeType.Fanout : ExchangeType.Direct, true, false, null);
+
+                channel.QueueDeclare(queue: "PSClientPays",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+                channel.QueueBind("PSClientPays", "PSClientPays", "");
+
+                string message = "Bolshakov message";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "PSClientPays",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
